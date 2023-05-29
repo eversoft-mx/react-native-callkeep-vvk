17bf748583e038a5ac7b84d7752d93315fc39fd0
Marcos <marcos_rbl_2000@outlook.com>
2023-05-29 15:23:11 -0600

    cambio f

--- android/src/main/java/io/wazo/callkeep/VoiceConnection.java
+++ android/src/main/java/io/wazo/callkeep/VoiceConnection.java
@@ -374,7 +374,7 @@ public class VoiceConnection extends Connection {
             mgr.createNotificationChannel(channel);
 
             Intent intent = new Intent(Intent.ACTION_MAIN, null);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NO_USER_ACTION | Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             intent.setClass(context, TestActivity.class);
             intent.putExtra("call_uuid",this.callUuid);
             intent.putExtra("caller_name",this.name);
